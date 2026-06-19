f3948d5a1cfe1d9f145e39d2279423ae1d203ee4
Stancu Anamaria <stancuanamaria0222@gmail.com>
2026-06-19 20:58:20 +0300

    Update CounterUpDown.sv

--- rtl/CounterUpDown.sv
+++ rtl/CounterUpDown.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/24/2026 12:24:41 PM
-// Design Name: 
-// Module Name: CounterUpDown
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module CounterUpDown(
     input logic clk,
     input logic rst,
