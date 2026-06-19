ed8bad614b23d81245d4ef22b47128508f6405ec
Stancu Anamaria <stancuanamaria0222@gmail.com>
2026-06-19 20:56:20 +0300

    Update SignalToggleFsm.sv
    Removed header comments from SignalToggleFsm.sv

--- rtl/SignalToggleFsm.sv
+++ rtl/SignalToggleFsm.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/24/2026 01:10:00 PM
-// Design Name: 
-// Module Name: SignalToggleFsm
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module SignalToggleFsm(
     input logic [7:0] in,
     input logic clk,
